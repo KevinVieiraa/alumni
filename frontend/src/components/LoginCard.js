d9d7c5e9d41ac98f5ed48ec963975c71ef956b3c
Kevin <kevin.vieira20@gmail.com>
2022-08-01 20:38:14 -0300

    setup inicial

---
+++ frontend/src/components/LoginCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './styles/LoginCard.module.css'
+
+function LoginCard() {
+    return (
+        <div class={styles.loginCard}>
+            <div class={styles.loginFormContainer}>
+                a
+            </div>
+            <div class={styles.welcomeMessageContainer}>
+                b
+            </div>
+        </div>
+    );
+}
+
+export default LoginCard;
